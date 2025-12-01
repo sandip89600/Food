ed572ba008101fac483fe00a620edbe809ad0429
Sandeep Pandit <sandippandit896@gmail.com>
2025-12-01 21:57:59 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './Verify.css'
 import {useNavigate, useSearchParams} from 'react-router-dom'
-import {StoreContext} from '../../context/StoreContext'
+import { StoreContext } from "../../Context/StoreContext.jsx";
 import axios from 'axios'
 import { useEffect } from 'react'
 
